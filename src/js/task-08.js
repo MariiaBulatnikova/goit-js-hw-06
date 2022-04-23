2d764b61d7636b7b462a0b9ccfb2cb3565360e33
dp200193bmv <92640930+dp200193bmv@users.noreply.github.com>
2022-04-23 22:23:54 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event)  {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '' ) {
+        alert('all fields must be filled!');
+    }
+    const formDataObj = {
+     email: email.value,
+        password: password.value,
+    };
+        console.log(formDataObj);
+        formEl.reset();
+}
+
+
 
