51b96b5683a279ff6b51a82a4b23047d616c8c80
dp200193bmv <92640930+dp200193bmv@users.noreply.github.com>
2022-04-24 19:37:07 +0300

    task 10

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,11 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    }`;
 }
 
 const btnEl = document.querySelector('.change-color');
 const spanEl = document.querySelector('.color')
+
+
 
 btnEl.addEventListener('click', () => {
   const body = document.querySelector('body');
