d7e055fee32bcb8627fe3b91912f557ee19c7bc6
dp200193bmv <92640930+dp200193bmv@users.noreply.github.com>
2022-04-25 21:33:18 +0300

    change task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,23 +1,10 @@
+const navlistEl = document.querySelector("#categories");
+console.log(`Number of categories: ${navlistEl.children.length} `);
 
-// const listEl = categories.children;
-
-
-// console.log('Number of categories:', listEl.length);
-// console.log('Category:', listEl[0].childNodes[1].innerText);
-// console.log('Elements:', listEl[0].childNodes[3].childElementCount);
-
-// console.log('Category:', listEl[1].childNodes[1].innerText);
-// console.log('Elements:', listEl[1].childNodes[3].childElementCount);
-
-// console.log('Category:', listEl[2].childNodes[1].innerText);
-// console.log('Elements:', listEl[2].childNodes[3].childElementCount);
-
-const listItemsEl = document.querySelectorAll('.item');
-// console.log(listItemsEl);
-
-console.log(`Number of categories: ${listItemsEl.length}`);
-
-const allCategoriesArray = listItemsEl.forEach(el => {
-    console.log(`Category: ${el.querySelector("h2").textContent}`);
-    console.log(`Elements: ${el.querySelectorAll("li").length}`);
+const listItemsEl = document.querySelectorAll(".item h2");
+listItemsEl.forEach((element) => {
+  console.log(`Category: ${element.textContent}`);
+  console.log(
+    `Elements: ${element.nextElementSibling.children.length}`
+  );
 });
